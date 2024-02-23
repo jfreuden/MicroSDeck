51cedc96658a929b2747d06e23ed9d06f4b4649a
jfreuden <jmf316@gmail.com>
2024-02-23 03:14:46 +0100

    Revert "Feat: Improve Documentation Engine Further (#17)"
    This reverts commit 5ba3afe8d41e1e01a09761638e0fadc4c7362e2b.

--- src/index.tsx
+++ src/index.tsx
@@ -150,13 +150,10 @@ export default definePlugin((serverApi: ServerAPI) => {
 	DeckyAPI.SetApi(serverApi);
 
 	Logger.Log("Started MicroSDeck");
-
+	
 	const patch = PatchAppScreen(serverApi);
 
-	serverApi.routerHook.addRoute(DOCUMENTATION_PATH, () => (
-		<MicroSDeckContextProvider microSDeck={window.MicroSDeck || (() => {throw "MicroSDeck not initialized";})()}>
-			<Docs />
-		</MicroSDeckContextProvider>));
+	serverApi.routerHook.addRoute(DOCUMENTATION_PATH, Docs);
 
 	return {
 		title: <div className={staticClasses.Title}>MicroSDeck</div>,
@@ -168,7 +165,7 @@ export default definePlugin((serverApi: ServerAPI) => {
 		onDismount() {
 			window.MicroSDeck?.destruct();
 			window.MicroSDeck = undefined;
-
+			
 			serverApi.routerHook.removeRoute(DOCUMENTATION_PATH);
 			patch && serverApi.routerHook.removePatch('/library/app/:appid', patch);
 		},
